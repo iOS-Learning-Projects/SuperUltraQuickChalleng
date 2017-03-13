520ef9f0284c0f2893ad58fb8bf7863a914804cf
Eduardo Vital Alencar Cunha <vital@Mac308.local>
2017-03-13 14:10:14 -0300

    Initial Commit

---
+++ Super Ultra Quick Challenge/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Super Ultra Quick Challenge
+//
+//  Created by Eduardo Vital Alencar Cunha on 13/03/17.
+//  Copyright © 2017 BEPiD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
